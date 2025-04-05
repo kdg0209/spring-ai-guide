90d1e2d630a79e176ae44e2534f63380f27fffaf
kdg0209 <s_cova_0418@naver.com>
2025-04-05 15:49:54 +0900

    init: project init

---
+++ src/main/java/com/woody/springai/guide/chapter01/chat/ChatController.java
@@ -0,0 +1,33 @@
+package com.woody.springai.guide.chapter01.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/chats")
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final OpenAiChatModel openAiChatModel;
+
+    @GetMapping("/ai/generate")
+    public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", openAiChatModel.call(message));
+    }
+
+    @GetMapping("/ai/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return openAiChatModel.stream(prompt);
+    }
+}
---
+++ src/test/java/com/woody/springai/guide/SpringAiGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woody.springai.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
